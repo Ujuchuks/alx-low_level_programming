4ae6fb2f917476c133921397733c166bbd827384
Ujuchuks <chukwujekwuobianuju@gmail.com>
2023-07-29 20:33:00 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - Entry point for the program
+*Return:  returns 0 for success
+*/
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
